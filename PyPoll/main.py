958a905513dc4a0234f712c80fabb0a8507912bb
Ryan Urabe <RyanUrabeToronto@gmail.com>
2023-06-28 00:09:35 -0400

    now done

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,44 +1,48 @@
 import os
 import csv
 
+# line 5 stores csv path in variable
 election_data = os.path.join("Resources", "election_data.csv")
 
-def election_results(data):
-    information = []
-    for row in data:
+def election_results(data):                             # defining fn to analyze data from csv
+    information = []                                    # define list variable to store data from csv in
+    for row in data:                                    # Loop to store data from csv in list
         list = [info for info in row]
         information.append(list)
     
-    total_votes = 0
-    candidates = []
+
+    total_votes = 0                                     # creates variable to hold total number of votes
+    candidates = []                                     # creaets list that will store candidates
 
 
-    for vote in information:
-        candidate = []
-        total_votes += 1
-        if len(candidates) < 1:
+    for vote in information:                            # loop to look at each list of data in information
+        candidate = []                                  # creates empty list meant to store one candidates data
+        total_votes += 1                                # counts total votes
+        if len(candidates) < 1:                         # if statement to see if there are no candidates yet observed, if so it adds one using the first data seen
             candidate.append(vote[2])
             candidate.append(1)
             candidates.append(candidate)
         
 
-        number = len(candidates)
+        number = len(candidates)                        # since len() doesnt work for lists when trying to for loop for it, need a variable to store length, variable will also help with skipping candidates not currently observed
 
-        for i in range(number):
-            
-            if vote[2].upper() == candidates[i][0].upper():
+        for i in range(number):                         # loop to check if candidate has been observed yet and loops for the amount of candidates observed 
+                                                        # I am aware that dictionaries could make this way simplier but I couldn't find the answer
+            if vote[2].upper() == candidates[i][0].upper():     # checks if candidate has been observed, if so adds vote to counter for candidate
                 candidates[i][1] += 1
 
             else:
-                number -= 1
-                if number == 0:
+                number -= 1                             # a unique way of doing things here. when only one candidate has been observed, the else statement wont be triggered. but if more than 1 have been observed, then i need this to skip candidates in list that don't match the current observed candidate
+                                                        # its a very unnecessarily convoluted way of going about it, but it works
+                if number == 0:                         
                     candidate.append(vote[2])
                     candidate.append(1)
                     candidates.append(candidate)
 
-    length = len(candidates)                
-    winner = candidates[0]
+    length = len(candidates)                            # same thing as line 27        
+    winner = candidates[0]                              # variable for winner info
     
+    # lines 46 to 63 are formatting for terminal output
     print('Election Results\n')
     for i in range(20):
         print('-', end='')
@@ -47,9 +51,9 @@ def election_results(data):
         print('-', end='')
     print('\n')
     for i in range(length):
-        if candidates[i][1] > winner[1]:
+        if candidates[i][1] > winner[1]:                # it statement to find candidate with most votes
             winner = candidates[i]
-        percentage = round((candidates[i][1] / total_votes)*100, 3)
+        percentage = round((candidates[i][1] / total_votes)*100, 3)     # formula to get percentage of vote that candidates achieved
         print(f'\n{candidates[i][0]}: {percentage}% ({candidates[i][1]})\n')
     for i in range(20):
         print('-', end='')
@@ -58,6 +62,7 @@ def election_results(data):
         print('-', end='')
     print('\n')
 
+    # line 65 creaetes a text file and allows for writing in it, lines 66 to 76 are formatting
     with open(os.path.join('analysis', 'Election Results'), 'w') as output:
         output.write('Election Results\n')
         output.write('------------------------------------\n')
@@ -71,9 +76,9 @@ def election_results(data):
         output.write('------------------------------------')
 
 
-with open(election_data, 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
+with open(election_data, 'r') as csvfile:               # opens csv 
+    csvreader = csv.reader(csvfile, delimiter=',')      # seperates data from csv by a ',' and puts it into one big list variable
 
-    header = next(csvreader)
+    header = next(csvreader)                            # takes header out of reading data
 
-    election_results(csvreader)
+    election_results(csvreader)                         # calls function from before using the csv data
--- Pybank/main.py
+++ Pybank/main.py
@@ -1,42 +1,45 @@
 import os
 import csv
 
+# line 5 stores csv path in variable
 budget_data_csv = os.path.join("Resources", "budget_data.csv")
 
-
-def run_analysis(data):
-    information = []
-    for row in data:
+def run_analysis(data):                             # defining fn to analyze data from csv
+    information = []                                # define list variable to store data from csv in
+    for row in data:                                # Loop to store data from csv in list
         list = [info for info in row]
         information.append(list)
     
             
-
-    months = 0
+    # lines 15-23 define variables
+    months = 0                              
     total_budget = 0
-    previous = int(information[0][1])
+    previous = int(information[0][1])               # previous is a variable that will be used to calculate change
     current_change = 0
     change = 0
     greatest_increase = 0
     month_increase = information[0][0]
     greatest_decrease = 0
     month_decrease = information[0][0]
-    for info in information:
-        months = months + 1
-        total_budget = total_budget + int(info[1])
-        current_change = (int(info[1]) - previous)
+
+    for info in information:                        # loop to go through all the data stored in 'information' 
+        months = months + 1                         # calculates number of months in data
+        total_budget = total_budget + int(info[1])  # adds profits (losses) from each month
+        current_change = (int(info[1]) - previous)  # current change is a variable used in two situations, calculating monthly change and finding greatest increase/decrease
         change = change + current_change
-        previous = int(info[1])
-        if current_change > greatest_increase:
+        previous = int(info[1])                     # sets preiovus to current data profit as its no longer needed in this loop but is needed for next month loop
+        if current_change > greatest_increase:      # finds greatest increase
             greatest_increase = current_change
             month_increase = info[0]
-        elif current_change < greatest_decrease:
+        elif current_change < greatest_decrease:    # finds greatest decrease
             greatest_decrease = current_change
             month_decrease = info[0]
-    average = round(change / (months - 1), 2)
 
-    print('Financial Analysis\n')
-    for i in range(20):
+    average = round(change / (months - 1), 2)       # average calculation
+
+    # from lines 41 to 48 is formating to print desired information to terminal
+    print('Financial Analysis\n')                   
+    for i in range(20):                             # loops '-' to not have to type '---------------------'
         print('-', end='')
     print('\n\nTotal Months: ' + str(months))
     print('\nTotal: $' + str(total_budget))
@@ -44,6 +47,7 @@ def run_analysis(data):
     print('\nGreatest Increase in Profits: ' + month_increase + ' ($' + str(greatest_increase) + ')')
     print(f'\nGreatest Decrease in Profits: {month_decrease} (${greatest_decrease})')
 
+    # line 52 creaetes a text file and allows for writing in it, lines 52 to 58 are formatting
     with open(os.path.join('analysis', 'Financial_Analysis_Output.txt'), 'w') as output:
         output.write('Financial Analysis\n')
         output.write('------------------------------------\n')
@@ -53,9 +57,9 @@ def run_analysis(data):
         output.write(f'Greatest Increase in Profits: {month_increase} (${greatest_increase})\n')
         output.write(f'Greatest Decrease in Profits: {month_decrease} (${greatest_decrease})\n')
 
-with open(budget_data_csv, "r") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
+with open(budget_data_csv, "r") as csvfile:         # opens csv 
+    csvreader = csv.reader(csvfile, delimiter=',')  # seperates data from csv by a ',' and puts it into one big list variable
 
-    header = next(csvreader)
+    header = next(csvreader)                        # takes header out of reading data
 
-    run_analysis(csvreader)
+    run_analysis(csvreader)                         # calls function from before using the csv data
